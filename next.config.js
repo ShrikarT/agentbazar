0560e8051e4febbb3496ca7bc000e3d076be1f73
DoD <shiva211111@gmail.com>
2026-07-12 17:33:22 +0530

    Fix NEXT_PUBLIC_API_URL for production

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || (process.env.NODE_ENV === 'production' ? "" : "http://localhost:8000"),
     NEXT_PUBLIC_EXPLORER: "https://preprod.cardanoscan.io/transaction",
   },
 }
